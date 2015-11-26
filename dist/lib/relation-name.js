bb958da9f47ade3573fc769ab7128719a4056d81
CircleCI <circleci@cureapp.jp>
2015-11-26 10:40:41 +0000

    v2.5.3

---
+++ dist/lib/relation-name.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(params) {
+  var foreignKey, keyThrough, model, through;
+  if (params == null) {
+    params = {};
+  }
+  model = params.model, foreignKey = params.foreignKey, keyThrough = params.keyThrough, through = params.through;
+  if (through) {
+    return model + '-via-' + keyThrough + '-at-' + through + '-with-' + foreignKey;
+  } else if (foreignKey) {
+    return model + '-with-' + foreignKey;
+  } else {
+    return model;
+  }
+};
